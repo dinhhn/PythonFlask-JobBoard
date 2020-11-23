22e731ad6bb1b82817f5e26a754720399379c7c9
Hieu Dinh <hieudinh00@gmail.com>
2020-11-23 00:55:21 -0600

    Completed module 6 - Display Individual Jobs

--- jobs/app.py
+++ jobs/app.py
@@ -20,6 +20,11 @@ def jobs():
     # In the body of the function return a call to the render_template() function, pass a parameter of 'index.html'.
     return render_template('index.html', jobs=jobs)
 
+@app.route('/job/<job_id>')
+def job(job_id):
+    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
+    return render_template('job.html', job=job)
+
 def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
