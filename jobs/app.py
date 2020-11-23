eafe3a9f0d43e8ae263054e8f99c0434b234a8b7
Hieu Dinh <hieudinh00@gmail.com>
2020-11-23 00:15:29 -0600

    Completed module 4 - Preparing for Dynamic Content

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,9 @@
 # import the Flask class and the render_template function from flask
-from flask import Flask
+from flask import Flask, g
 from flask import render_template
+import sqlite3
+
+PATH = "db/jobs.sqlite"
 
 # create an instance of the Flask class called app. Pass the special variable __name__ to the Flask class constructor.
 app = Flask(__name__)
@@ -15,4 +18,30 @@ app = Flask(__name__)
 def jobs():
     # In the body of the function return a call to the render_template() function, pass a parameter of 'index.html'.
     return render_template('index.html')
+
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection == None:
+        connection = sqlite3.connect(PATH)
+        g._connection = connection
+    connection.row_factory = sqlite3.Row
+    return connection
+
+def execute_sql(sql, values = (), commit = False, single = False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+    cursor.close()
+    return results
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection != None:
+        connection.close()
+    
+
 
